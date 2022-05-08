b055ceb060e384e547201719d01c7b1a0df7cb50
a-cole55 <arniqua_c@yahoo.com>
2022-05-08 12:38:19 -0500

    added animations to services section

--- md-js.js
+++ md-js.js
@@ -1,3 +1,4 @@
+
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 window.onscroll = function() {scrollFunction()};
@@ -11,8 +12,12 @@ function scrollFunction() {
 }
 
 let scrollpos = window.scrollY; // window scroll position
-let wh = window.innerHeight-50; // as soon as element touches bottom with offset event starts
+console.log(scrollpos)
+let wh = window.innerHeight+250; // as soon as element touches bottom with offset event starts
+console.log(wh)
 let element = document.querySelector("#about"); //element
+console.log(element.offsetTop)
+console.log(element.offsetTop - wh)
 
 window.addEventListener('scroll', function(){ 
     if(scrollpos > (element.offsetTop - wh)){
@@ -20,31 +25,21 @@ window.addEventListener('scroll', function(){
     }
 });
 
-
-
 // Services Section
-document.querySelector('#link').addEventListener('click', display1)
-document.querySelector('#link2').addEventListener('click', display2)
-document.querySelector('#link3').addEventListener('click', display3)
+document.querySelector('#link').addEventListener('mouseover', display1)
+document.querySelector('#link2').addEventListener('mouseover', display2)
+document.querySelector('#link3').addEventListener('mouseover', display3)
 
 function display1(){
-  if(document.querySelector('#sec1').style.display == 'none'){
-    document.querySelector('#sec1').style.display = 'block'
-  } else{
-  document.querySelector('#sec1').style.display = 'none'
-}
+  let sec1 = document.querySelector('#sec1')
+  sec1.classList.add("fade");
+  sec1.style.display = 'block';
 }
 function display2(){
-    if(document.querySelector('#sec2').style.display == 'none'){
-      document.querySelector('#sec2').style.display = 'block'
-    } else {
-      document.querySelector('#sec2').style.display = 'none'
-    }
+    document.querySelector('#sec2').style.display = 'block';
+    document.querySelector('#sec2').classList.add("fade");
 }
 function display3(){
-    if(document.querySelector('#sec3').style.display == 'none'){
-      document.querySelector('#sec3').style.display = 'block'
-    } else{
-      document.querySelector('#sec3').style.display = 'none'
-    }
+  document.querySelector('#sec3').style.display = 'block';
+  document.querySelector('#sec3').classList.add("fade");
 }
